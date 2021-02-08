f40aa1b9a65b1e30799767cab17ba2aed5b45ea4
Steve <steve.goering@tu-ilmenau.de>
2021-02-08 14:38:06 +0100

    changes ins cript

--- predict_all.sh
+++ predict_all.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#./predict.py --image_folder data/avt_image_db/jpg/ \
-#    --report_name prediction_rule_of_thirds.json \
-#    --mode rule_of_thirds/models/ResNet152_best_model.hdf5
+./predict.py --image_folder data/avt_image_db/jpg/ \
+    --report_name prediction_rule_of_thirds.json \
+    --mode rule_of_thirds/models/ResNet152_best_model.hdf5
 
 ./predict.py --image_folder data/avt_image_db/jpg/ \
     --report_name prediction_simplicity.json \
